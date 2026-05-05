b4cbd5bb5a02aedf23483251d6a88a44bcfcc5eb
Bhumika K P <bhumikagowda008@gmail.com>
2026-05-06 01:53:37 +0530

    fix: firebase env setup

--- firebase_config.py
+++ firebase_config.py
@@ -1,7 +1,15 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+import json
 
-cred = credentials.Certificate("firebase-key.json")
-firebase_admin.initialize_app(cred)
+firebase_key = os.getenv("FIREBASE_KEY")
+
+if firebase_key:
+    cred_dict = json.loads(firebase_key)
+    cred = credentials.Certificate(cred_dict)
+    firebase_admin.initialize_app(cred)
+else:
+    raise Exception("Firebase key not found")
 
 db = firestore.client()
